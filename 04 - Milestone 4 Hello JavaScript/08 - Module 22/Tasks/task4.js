cb78dd8b6ea04f71dbe55c1e40ac5d6db1a531fa
MithuAhammad <mithuahammad2@gmail.com>
2024-11-19 23:48:25 +0600

    09 - Module 23

--- 04 - Milestone 4 Hello JavaScript/08 - Module 22/Tasks/task4.js
+++ 04 - Milestone 4 Hello JavaScript/08 - Module 22/Tasks/task4.js
@@ -7,17 +7,16 @@ sample-input: I am learning Programming to become a programmer
 sample-output: Programming
 */
 
-
 function largestWord(string) {
-    let words = string.split(' ');
-    let longestWord = "";
+  let words = string.split(" ");
+  let longestWord = "";
 
-    for (const word of words) {
-        if (word.length > longestWord.length) {
-            longestWord = word;
-        };
-    };
-    console.log(longestWord);
+  for (const word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  console.log(longestWord);
 }
 
-largestWord('I am learning Programming to become a programmer');
+largestWord(" I am learning Programming to become a programmer");
